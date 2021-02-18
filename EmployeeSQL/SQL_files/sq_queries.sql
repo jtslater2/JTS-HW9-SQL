343ca386ab6879a6b0aaf6b3490e4613254e13e1
unknown <jtslater2@gmail.com>
2021-02-17 21:57:42 -0500

    updates 10:00

--- EmployeeSQL/SQL_files/sq_queries.sql
+++ EmployeeSQL/SQL_files/sq_queries.sql
@@ -5,11 +5,31 @@
 --Select * from salaries
 --Select * from titles
 
---  Q1
+-- Data Analysis #1
 
-CREATE VIEW "employee information" AS
+CREATE VIEW "employee_info" AS
 SELECT employees.emp_no, last_name, first_name, sex, salaries.salary
 FROM employees
 INNER JOIN salaries ON employees.emp_no = salaries.emp_no;
 
-SELECT * from "employee information"
+SELECT * from "employee_info"
+
+-- Data Analysis #2
+
+CREATE VIEW "hire_1986" AS
+SELECT employees.first_name, last_name, hire_date
+FROM employees
+WHERE hire_date Between '1986-01-01' AND '1986-12-31'
+
+SELECT * from "hire_1986"
+
+-- Data Analysis #3
+
+CREATE VIEW "mgr_info" AS
+SELECT d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM departments AS d
+INNER JOIN dept_manager AS dm ON dm.dept_no = d.dept_no
+INNER JOIN employees AS e ON dm.emp_no = e.emp_no;
+
+SELECT * from "mgr_info"
+
